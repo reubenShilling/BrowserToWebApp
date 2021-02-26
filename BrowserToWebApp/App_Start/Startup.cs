410eb2088d17dfc59e7992fd13abe40e03a6b1e1
ReubenShilling <chiefthunderfoot@gmail.com>
2021-02-26 15:40:39 -0600

    Initial commit

---
+++ BrowserToWebApp/App_Start/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+
+[assembly:OwinStartup(typeof(BrowserToWebApp.Startup))]
+namespace BrowserToWebApp
+{
+    public class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+        
+        string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthenticationFailed = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("/Error/message=" + context.Exception.Message);
+                            return Task.FromResult(0);
+                        }
+                    }
+                });
+        }
+    }
+}
---
+++ BrowserToWebApp/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+namespace BrowserToWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties {RedirectUri = "/"},
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                CookieAuthenticationDefaults.AuthenticationType);
+        }
+    }
+}
